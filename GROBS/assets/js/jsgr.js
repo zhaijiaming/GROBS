77e999bcb39de961ba6cb2deb686d0b8e81b6e70
Administrator <Administrator@MS-20160819UUIV>
2017-07-26 10:00:27 +0800

    主页面修改

--- GROBS/assets/js/jsgr.js
+++ GROBS/assets/js/jsgr.js
@@ -1,12 +1,24 @@
-﻿window.onload = function () {
-    var swiper = new Swiper('.swiper-container', {
-        autoplay: 5000,
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        slidesPerView: 1,
-        paginationClickable: true,
-        loop: true
+﻿$(document).ready(function () {
 
+
+    $('#myCarousel').carousel({
+        //设置自动播放/2 秒
+        interval: 3000,
     });
-}
+
+
+    $('.carousel-control').css('line-height', $('.carousel-innerimg').height() + 'px');
+    $(window).resize(function () {
+        var $height = $('.carousel-inner img').eq(0).height() || $('.carousel-inner img').eq(1).height() || $('.carousel-inner img').eq(2).height();
+        $('.carousel-control').css('line-height', $height + 'px');
+    });
+
+    $(".left").click(function () {
+        $("#myCarousel").carousel('prev');
+    });
+    // 循环轮播到下一个项目
+    $(".right").click(function () {
+        $("#myCarousel").carousel('next');
+    });
+
+});
